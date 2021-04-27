a70f3ec9ec64d788286c59f47abeffca06164c40
AG1L1TYx7 <bishworupx7@gmail.com>
2021-04-27 11:00:38 +0545

    full project

---
+++ frontend/frontend/src/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'GPU',
+      category: 'GPU',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' ASUS',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'gpu',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Samsung',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'memory',
+      category: 'MSI',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: 'DELL',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'Ram',
+      category: 'memory',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: 'HP',
+      rating: 4.5,
+      numReviews: 8
+    },
+  ]
+}
